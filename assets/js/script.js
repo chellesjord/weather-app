50ccfa8d64650958bec721a75b06832b722a1a6f
chellesjord <m.kayejordan@gmail.com>
2021-12-12 22:43:54 -0600

    5 day display

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,6 @@ var futureContainerEl = document.querySelector("#futureForcast-container");
 var futureSearchTerm = document.querySelector("#futureForcast-search-term");
 var currentTemperature = document.querySelector("#currentTemp");
 
-
-//append dates to future days
-// var futureDay1 = moment().add(1, "day").format("MM/DD/YYYY"));
-// var futureDay2 = moment().add(2, "day").format("MM/DD/YYYY"));
-// var futureDay3 = moment().add(3, "day").format("MM/DD/YYYY"));
-// var futureDay4 = moment().add(4, "day").format("MM/DD/YYYY"));
-// var futureDay5 = moment().add(5, "day").format("MM/DD/YYYY"));
-
 var formSubmitHandler = function (event) {
     //prevent page from refreshing
     event.preventDefault();
@@ -24,7 +16,6 @@ var formSubmitHandler = function (event) {
         getCityWeekForcast(city);
 
         //clear old content
-        futureContainerEl.textContent = "";
         cityInputEl.value = "";
     } else {
         alert("Please enter a city")
@@ -39,7 +30,6 @@ var getCityDayForcast = function (cityName) {
             //request was successful
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
 
                     document.getElementById("dayBlock").setAttribute("class", "dayBlock");
 
@@ -70,40 +60,63 @@ var getCityDayForcast = function (cityName) {
 };
 
 var getCityWeekForcast = function (cityName) {
-    var apiURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&cnt=6&appid=afc02854dff0cf33f3674d9fbe5474da";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=afc02854dff0cf33f3674d9fbe5474da";
 
-    for (var i = 1; i < 6; i++) {
-        var futureDayName = moment().add(i, "day").format("MM/DD/YYYY");
-        console.log(futureDayName);
+    fetch(apiUrl)
+        .then(function (response) {
+            //request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
 
-        //         //create a container for each day
-        //         var futureEl = document.createElement("div");
-        //         futureEl.classList = "col-2 forcastDays futureForecast-container"
 
-        //         //create a span element to hold city name
-        //         var titleEl = document.createElement("span");
-        //         titleEl.textContent = futureDayName;
+                    var fivedayapi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly&cnt=6&units=imperial&appid=afc02854dff0cf33f3674d9fbe5474da";
 
-        //         //append to container
-        //         futureEl.appendChild(titleEl);
+                    fetch(fivedayapi)
+                        .then(function (response) {
+                            //request was successful
+                            if (response.ok) {
+                                response.json().then(function (data) {
+                                    console.log(data);
 
-        //         //creat a status element
-        //         var statusEl = document.createElement("span");
-        //         statusEl.classList = "flex-row alrign-center";
+                                    if (data.daily[0].uvi <= 2) {
+                                        document.getElementById("UV").innerHTML = "UV Index: <span class='greenbg'>" + data.daily[0].uvi + "</span>";
+                                    }
+                                    else if (data.daily[0].uvi <= 7) {
+                                        document.getElementById("UV").innerHTML = "UV Index: <span class='yellowbg'>" + data.daily[0].uvi + "</span>";
+                                    }
+                                    else {
+                                        document.getElementById("UV").innerHTML = "UV Index: <span class='redbg'>" + data.daily[0].uvi + "</span>";
+                                    }
 
-        //         //append container to the dom
-        //         futureContainerEl.appendChild(futureEl);
-
-        //         // <div class="col-2 forcastDays" id="futureForcast-container">
-        //         //     <p class="date" id="fdate1"></p>
-        //         //     <p class="weatherIcon">ICON</p>
-        //         //     <p class="futureTemp">54degress</p>
-        //         //     <p class="futureWind">12 MPH</p>
-        //         //     <p class="futureHumidity">55%</p>
-        //         // </div>
-        //     }
-        // };
-    }
+                                    for (i = 1; i < 6; i++) {
+                                        // append dates to future dates
+                                        document.getElementById("fdate" + i).innerHTML = moment().add(i, "day").format("MM/DD/YYYY");
+                                        // append weather icons to future dates
+                                        document.getElementById("img" + i).src = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+                                        // append temperatures for future dates
+                                        document.getElementById("temp" + i).innerHTML = data.daily[i].temp.day + "°F";
+                                        // append wind for future dates
+                                        document.getElementById("wind" + i).innerHTML = data.daily[i].wind_speed + " MPH";
+                                        // append humidity for future dates
+                                        document.getElementById("hum" + i).innerHTML = data.daily[i].humidity + " %";
+                                    }
+                                });
+                            } else {
+                                alert("Error: " + response.statusText)
+                            }
+                        })
+                        .catch(function (error) {
+                            alert("Unable to connect to WeatherAPI");
+                        });
+                });
+            }
+            else {
+                alert("Error: " + response.statusText)
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to connect to WeatherAPI");
+        });
 };
 
 //add event listeners to search
